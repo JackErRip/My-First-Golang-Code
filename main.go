5bb5f95e00b44f7493aede3ec21d65f05125b540
JackErRip <136085120+JackErRip@users.noreply.github.com>
2023-06-10 10:53:37 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+func main() {
+
+	//Variables
+	var game string = "DEAD ISLAND 2"
+	var face string = "please please please :)"
+	var price int = 60
+	var moneysymbol string = "$$$$$$"
+	var nitromes string = "please buy me discord nitro one year"
+	var nitroprice float32 = 2639.99
+	var MyGPUSize int = 12
+	var matthewGPUSize int = 11
+
+	//constants
+	const gameprize = 60
+
+	//print line
+	println("Hello Brad")
+	println("Can You Buy Me ")
+	println(game)
+	println(face)
+	println(price)
+	println(moneysymbol)
+	println("could I get it for 60 - 60 =")
+	println(price - gameprize)
+	println("DOLLARS or PESOS")
+	println("please buy me discord nitro one year plessssssssss")
+	println(nitroprice)
+	println("PESOS")
+	println(nitromes)
+
+	println("If my gpu's size is 12 inches and matthew's ps4 gpu's size is at least 1 inch. Then who has the bigger size?")
+	if MyGPUSize >= matthewGPUSize {
+		println("My DIck Is WaY bigGEr than matthew")
+	} else if {
+        println("WE BOTH HAVE the same size LOL :)")
+	} else {
+		println("matthew's dick is way bigger than aldrien")
+	}
+}
